b816334e876ddaeb9e9594e6974e5707ae313d1e
dummy123king <dummy123king@gmail.com>
2024-02-06 14:58:36 +0530

    Added LSP

---
+++ LinuxSystemProgramming/FileOperations.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+    int fd, dataReadLen = 0;
+    char buff[1000];
+    char dataTobeWritten[] = "This Data has written to the file";
+
+    fd = open("test.txt", O_RDWR, 07777); //Open the file in Read and write mode
+
+    if (fd == -1)
+    {
+        printf("Error occured : %d\n", errno);
+        perror("Erro");
+        return -1;
+    }
+
+    dataReadLen = read(fd, buff, 1000);// Read data from test.txt file.
+    if (dataReadLen == 0)
+    {
+        printf("Reached EOF\n");
+    }
+
+    printf("Size : %d\n", dataReadLen);
+    buff[dataReadLen] = '\0';
+    printf("data : %s\n", buff);
+
+
+    if (write(fd, dataTobeWritten, strlen(dataTobeWritten)) == -1)
+    {
+        perror("Error");
+    }
+    
+    lseek(fd, 0, 0);
+    dataReadLen = read(fd, buff, 1000);// Read data from test.txt file.
+    if (dataReadLen == 0)
+    {
+        printf("Reached EOF\n");
+    }
+    
+    printf("Size : %d\n", dataReadLen);
+    buff[dataReadLen] = '\0';
+    printf("data : %s\n", buff);
+    
+    close(fd);
+
+    return 0;
+}
